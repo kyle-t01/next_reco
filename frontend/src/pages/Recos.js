e2c2dad30dcae8596ad79976afaeed8588c8ee6f
kylet-01 <userEmail>
2025-02-24 16:21:53 +1100

    recos page after user has signed in

---
+++ frontend/src/pages/Recos.js
@@ -0,0 +1,14 @@
+// import { useEffect, useState } from "react";
+// components
+
+
+const Recos = () => {
+
+    return (
+        <div className="recos">
+            <h1>This is where you can see all Recos</h1>
+        </div>
+    );
+}
+
+export default Recos
