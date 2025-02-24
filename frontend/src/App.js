1e581eb0817da079553f78701f013a495aaf8cc2
kylet-01 <userEmail>
2025-02-24 11:59:38 +1100

    frontend folder for react

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Home</p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
