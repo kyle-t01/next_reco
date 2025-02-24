a60d4633b30850909c8886563d828d5026bc7452
kylet-01 <userEmail>
2025-02-24 15:50:21 +1100

    can now logout via navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,10 +4,20 @@ import { UserAuth } from "../context/AuthContext";
 const Navbar = () => {
     const { user, logOut } = UserAuth()
 
+    const handleLogOut = async () => {
+        try {
+            await logOut()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+
     return (
         <div className="navbar">
             <h1>NextReco</h1>
-            {user ? <button>Logout</button> : <Link to='/signin'>Sign In</Link>}
+            {user ? <button onClick={handleLogOut}>Logout</button> : <Link to='/signin'>Sign In</Link>}
         </div>
     )
 }
