d60afc281465fc5948c82e37da8e85d2d0116d6e
kylet-01 <userEmail>
2025-02-24 12:01:50 +1100

    basic homepage

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,16 @@
+// import { useEffect, useState } from "react";
+// components
+
+
+const Home = () => {
+
+    return (
+        <div className="home">
+            <h1>Welcome to NextReco!</h1>
+            <p>"Let's do this next!"</p>
+            <button>Sign in with Google</button>
+        </div>
+    );
+}
+
+export default Home
