de765138964e238206285a650a830b44b0ea309c
kylet-01 <userEmail>
2025-02-25 18:35:04 +1100

    Reco page will fetch all personal recos at first load

--- frontend/src/pages/Recos.js
+++ frontend/src/pages/Recos.js
@@ -1,11 +1,19 @@
-// import { useEffect, useState } from "react";
-// components
-
-// need to redirect user to the home page when not signed in 
-
+import { useEffect, useState } from "react";
+import { getRecos } from '../services/recoServices'
+import { UserAuth } from '../context/AuthContext'
 
 const Recos = () => {
+    const { user } = UserAuth()
+    const [personalRecos, setPersonalRecos] = useState(null)
 
+    useEffect(() => {
+        const getPersonalRecos = async (user) => {
+            const data = await getRecos(user)
+            console.log(data)
+        }
+        getPersonalRecos(user)
+
+    }, []);
     return (
         <div className="recos">
             <h1>This is where you can see all Recos</h1>
