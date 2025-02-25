2eca021d10e22c7894c7e37a4e7970dd455b7558
kylet-01 <userEmail>
2025-02-25 20:49:15 +1100

    post method added

--- frontend/src/services/recoServices.js
+++ frontend/src/services/recoServices.js
@@ -1,9 +1,9 @@
 /* set of helper functions connecting backend and frontend*/
 
 
-const fetchRecos = async (user, method) => {
+const fetchRecos = async (user, method, body = null) => {
 
-    console.log("fetching Recos for user: ", user, " via: ", method)
+    console.log("fetching Recos for user: ", user.uid, " via: ", method)
 
 
 
@@ -14,11 +14,14 @@ const fetchRecos = async (user, method) => {
         // request headers to contain idToken
         const headers = {
             'Authorization': `Bearer ${idToken}`,
+            "Content-Type": "application/json",
         };
 
         const response = await fetch('http://localhost:4000/api/recos', {
             method: method,
             headers: headers,
+            // only add a body if not null
+            ...(body ? { body: JSON.stringify(body) } : {}),
         });
 
 
@@ -27,6 +30,7 @@ const fetchRecos = async (user, method) => {
             const data = await response.json()
             return data
         } else {
+
             return null
         }
     } catch (error) {
@@ -40,5 +44,10 @@ const fetchRecos = async (user, method) => {
 
 export const getRecos = async (user) => {
     const data = await fetchRecos(user, "GET");
+    return data;
+}
+
+export const createReco = async (user, newReco) => {
+    const data = await fetchRecos(user, "POST", newReco);
     return data;
 }
