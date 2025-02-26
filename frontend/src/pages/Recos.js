09f2b3365ec6549a7071ac50faf87c1ac374caf1
kylet-01 <userEmail>
2025-02-26 13:16:39 +1100

    display a list of recos

--- frontend/src/pages/Recos.js
+++ frontend/src/pages/Recos.js
@@ -3,6 +3,7 @@ import { getRecos } from '../services/recoServices'
 import { UserAuth } from '../context/AuthContext'
 
 import RecoForm from "../components/RecoForm";
+import RecoGridDisplay from "../components/RecoGridDisplay";
 
 const Recos = () => {
     const { user } = UserAuth()
@@ -13,13 +14,18 @@ const Recos = () => {
         const getPersonalRecos = async (user) => {
             const data = await getRecos(user)
             console.log(data)
+            setPersonalRecos(data)
         }
         getPersonalRecos(user)
 
     }, []);
+
+
+
     return (
         <div className="recos">
             <h1>This is where you can see all Recos</h1>
+            <RecoGridDisplay recos={personalRecos} />
             <button onClick={() => setIsFormOpen(true)}>+ Add Reco</button>
             <RecoForm isOpen={isFormOpen} onClose={() => setIsFormOpen(false)} onRecoAdded={() => { }} />
         </div>
