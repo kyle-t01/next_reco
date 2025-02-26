b01aabb53a742fd5116a6a385891867af816da07
kylet-01 <userEmail>
2025-02-26 11:35:42 +1100

    Reco page to have reco form popup

--- frontend/src/pages/Recos.js
+++ frontend/src/pages/Recos.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { getRecos } from '../services/recoServices'
 import { UserAuth } from '../context/AuthContext'
 
+import RecoForm from "../components/RecoForm";
+
 const Recos = () => {
     const { user } = UserAuth()
     const [personalRecos, setPersonalRecos] = useState(null)
+    const [isFormOpen, setIsFormOpen] = useState(false)
 
     useEffect(() => {
         const getPersonalRecos = async (user) => {
@@ -17,6 +20,8 @@ const Recos = () => {
     return (
         <div className="recos">
             <h1>This is where you can see all Recos</h1>
+            <button onClick={() => setIsFormOpen(true)}>+ Add Reco</button>
+            <RecoForm isOpen={isFormOpen} onClose={() => setIsFormOpen(false)} onRecoAdded={() => { }} />
         </div>
     );
 }
