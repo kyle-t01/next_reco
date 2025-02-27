14ec87901c2f12b8950fdf7543c8046ed7c45bbe
kylet-01 <userEmail>
2025-02-27 11:39:40 +1100

    reco box now displays sub titles

--- frontend/src/components/RecoBox.js
+++ frontend/src/components/RecoBox.js
@@ -3,19 +3,20 @@
 
 
 const RecoBox = ({ reco }) => {
-    const { title, description, googleImageUrl } = reco;
+    const { title, subTitle, googleImageUrl } = reco;
     return (
         <div className="reco-box">
             {/* Title */}
             <h4 className="reco-title">{title}</h4>
-
+            {/* Sub title */}
+            <p> {subTitle}</p>
             {/* Display Image */}
             {googleImageUrl ? (
                 <img src={googleImageUrl} alt={title} className="reco-image" />
             ) : (
                 <div className="image-placeholder">No Image Available</div>
             )}
-            
+
 
 
         </div>
