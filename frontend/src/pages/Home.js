83042bc86ee439e13d18c442dbf3e92382c8ae76
kylet-01 <userEmail>
2025-02-28 12:27:58 +1100

    redirect to recos when already signed in

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,19 @@
 // import { useEffect, useState } from "react";
 // components
-
-
+import { useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
+import { useEffect, useState } from "react";
 const Home = () => {
+    const { user } = UserAuth()
+    const navigate = useNavigate()
+    useEffect(() => {
 
+        if (user != null) {
+            // already signed in 
+            console.log("already signed in")
+            navigate('/recos')
+        }
+    }, [user]);
     return (
         <div className="home">
             <h1>Welcome to NextReco!</h1>
