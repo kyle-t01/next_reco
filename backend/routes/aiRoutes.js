c75a0437d518b9f8c1cb6069c231bdc2fece6a11
kylet-01 <userEmail>
2025-03-04 18:18:56 +1100

    addeed a system prompt to ai

--- backend/routes/aiRoutes.js
+++ backend/routes/aiRoutes.js
@@ -18,13 +18,32 @@ router.use(authMW)
 router.post('/', async (req, res) => {
 
     console.log("### ANALYSING AI PROMPT ###")
-    const body = req.body
-    console.log("req.body", body)
+    const { prompt, reco } = req.body
+    console.log("The prompt recieved was: ", prompt)
+    console.log("The reco recieved was: ", reco)
+
+    // prompt engineering for system prompt
+    // input: unstructured data => output: reco object
+    const systemPrompt = "You are an ai that extracts structured data from unstructured user requests. Your task is to parse the user query into a JSON object formatted as a Reco. A Reco should include -title (string,required) -subTitle(string, optional) -category(specificallyeither food or non-food) -isPrivate(boolean, default false) -isProposed(boolean, default false) -googleData(null) -isLookup(boolean). Additionally categorise the user request into [manual] if the user specifies a custom name and address, or [lookup] "
+
+    // check whether there was a prompt
+    if (!prompt || prompt.trim() === "") {
+        return res.status(400).json({ error: "Prompt was empty!" });
+    }
+
+    // give the ai some context
+    if (reco) {
+        console.log("should add some context prompts to AI")
+    }
 
     try {
+        const aiResponse = await openai.chat.completions.create({
+            model: "gpt-4o-mini",
+            messages: [{ role: "user", content: prompt }, { role: "system", content: systemPrompt }],
+        });
 
-        console.log("success in an AI prompt")
-        res.status(200).json(body);
+
+        res.json({ result: aiResponse.choices[0].message.content });
     } catch (error) {
         console.log(error)
         res.status(400).json({ error: error.message });
