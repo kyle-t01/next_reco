7db0b87c0a970a937bd7780b59ce4a9f06e6a0cb
kylet-01 <userEmail>
2025-03-05 19:41:06 +1100

    backend handles finding google placeID from a search text

--- backend/routes/googleRoutes.js
+++ backend/routes/googleRoutes.js
@@ -84,8 +84,46 @@ router.get('/image', async (req, res) => {
     }
 });
 
+// try to get placeID from searchText
+router.get('/search', async (req, res) => {
+
+    console.log("### FETCHING GOOGLE PLACE_ID FROM SEARCHTEXT ###")
+    if (!req || !req.query) {
+        return res.status(400).json({ error: "googleRoutes/search : missing query params" });
+    }
 
 
+    const { searchText } = req.query;
 
+
+    // check whether there was a searchText supplied
+    if (!searchText || searchText === "") {
+        return res.status(400).json({ error: "searchText was empty!" });
+    }
+
+
+    try {
+        const apiKey = process.env.GOOGLE_PLACES_API_KEY;
+        const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${encodeURIComponent(searchText)}&inputtype=textquery&fields=place_id&key=${apiKey}`;
+
+        const response = await axios.get(url);
+
+        if (response.data.status !== "OK") {
+            console.error("Google Places API Error:", response.data.status);
+            return res.status(400).json({ error: response.data.status });
+        }
+
+        const placeID = response.data.candidates[0].place_id;
+
+
+        console.log("Google Place ID found:", placeID);
+        res.status(200).json(placeID);
+
+    } catch (error) {
+        console.error("Error fetching Google Place ID:", error);
+        res.status(500).json({ error: "Failed to fetch place ID" });
+    }
+
+});
 
 module.exports = router;
