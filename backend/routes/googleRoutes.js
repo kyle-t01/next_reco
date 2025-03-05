e0d96a3d70f20b458f75606d0adc40f1a185c7b2
kylet-01 <userEmail>
2025-03-05 15:13:24 +1100

    installed axios

---
+++ backend/routes/googleRoutes.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+const express = require('express')
+const axios = require('axios')
+const router = express.Router();
+
+// don't use authentication
+// router.use(authMW)
+
+// handle requests to get place details
+router.get('/', async (req, res) => {
+
+    console.log("### FETCHING GOOGLE PLACE DETAILS ###")
+    if (!req || !req.query) {
+        return res.status(400).json({ error: "getRecos: missing query params" });
+    }
+
+
+    const { placeID } = req.query;
+
+
+    // check whether there was a placeID supplied
+    if (!placeID || placeID === "") {
+        return res.status(400).json({ error: "PlaceID was empty!" });
+    }
+
+
+    try {
+        const apiKey = process.env.GOOGLE_PLACES_API_KEY;
+        const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&key=${apiKey}`;
+
+        const response = await axios.get(url);
+        console.log(response)
+        if (response.data.status !== "OK") {
+            console.error("Google Places API Error:", response.data.status);
+            return res.status(400).json({ error: response.data.status });
+        }
+
+        console.log("Google Place Data:", response.data);
+        res.status(200).json(response.data);
+
+    } catch (error) {
+        console.error("Error fetching Google Place details:", error);
+        res.status(500).json({ error: "Failed to fetch place details" });
+    }
+
+});
+
+
+module.exports = router;
