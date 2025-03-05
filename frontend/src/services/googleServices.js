ed604231ce09099c4c7c45872c7cff5053498897
kylet-01 <userEmail>
2025-03-05 19:56:50 +1100

    added fetch place id from text function

--- frontend/src/services/googleServices.js
+++ frontend/src/services/googleServices.js
@@ -32,3 +32,38 @@ export const fetchGoogleData = async (placeId) => {
 
 
 }
+
+// find a placeID from an address text
+export const fetchPlaceIDFromText = async (text) => {
+    console.log("fetching a placeID from text...")
+    const searchText = text;
+
+
+    if (!searchText || searchText.trim() === "") return null;
+
+    try {
+
+        // request options
+        const requestOptions = {
+            method: "GET",
+        };
+
+
+        const url = `https://next-reco-app.onrender.com/api/google/search?searchText=${encodeURIComponent(searchText)}`;
+        console.log("url: ", url)
+        const response = await fetch(url, requestOptions);
+
+        if (response.ok) {
+
+            const data = await response.json()
+            return data
+        } else {
+            console.error(`fetchPlaceIDFromText ERROR`);
+            return null
+        }
+    } catch (error) {
+        console.log("error:", error);
+    }
+
+
+}
