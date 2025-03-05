92c1e0abeb0cf0d0202b56cac06b91523d7656fc
kylet-01 <userEmail>
2025-03-05 16:05:47 +1100

    only extract required fields from place details

--- backend/routes/googleRoutes.js
+++ backend/routes/googleRoutes.js
@@ -29,14 +29,29 @@ router.get('/', async (req, res) => {
         const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&key=${apiKey}`;
 
         const response = await axios.get(url);
-        console.log(response)
+
         if (response.data.status !== "OK") {
             console.error("Google Places API Error:", response.data.status);
             return res.status(400).json({ error: response.data.status });
         }
+        const result = response.data.result;
+        const googleData = {
+            name: result.name,
+            address: result.formatted_address,
+            phone: result.formattted_phone_number,
+            //editorialSummary: result.editorial_summary?.overview ||  null,
+            imageURL: result.photos ? result.photos[0]?.html_attributions[0] : null,
+            priceLevel: result.price_level,
+            rating: result.rating,
+            website: result.website,
+            url: result.url,
+            userRatingsTotal: result.user_ratings_total,
+            vicinity: result.vicinity
+        }
 
-        console.log("Google Place Data:", response.data);
-        res.status(200).json(response.data);
+
+        console.log("Google Place Data:", googleData);
+        res.status(200).json(googleData);
 
     } catch (error) {
         console.error("Error fetching Google Place details:", error);
