9bf8bd65b114b3aeb286fd1feb1d693e8f144dec
kylet-01 <userEmail>
2025-03-06 20:04:34 +1100

    connecting to the POST /api/ai/subset endpoint

--- frontend/src/services/aiServices.js
+++ frontend/src/services/aiServices.js
@@ -45,3 +45,43 @@ export const fetchAIResponse = async (user, reco, prompt) => {
 
 
 
+export const fetchAIResponseSubset = async (user, recos, prompt) => {
+    console.log("fetching AI response...(subset of recos)")
+    try {
+        // get current id token of user
+        const idToken = await user.getIdToken();
+
+        // request headers to contain idToken
+        const headers = {
+            'Authorization': `Bearer ${idToken}`,
+            "Content-Type": "application/json",
+        };
+
+        // request options
+        const requestOptions = {
+            method: "POST",
+            headers: headers,
+        };
+
+        requestOptions.body = JSON.stringify({
+            prompt: prompt,
+            recos: recos || null
+        });
+
+        const url = `https://next-reco-app.onrender.com/api/ai/subset`;
+        console.log("url: ", url)
+        const response = await fetch(url, requestOptions);
+
+        if (response.ok) {
+
+            const data = await response.json()
+            return data
+        } else {
+            console.error(`ERROR`);
+            return null
+        }
+    } catch (error) {
+        console.log("error:", error);
+    }
+
+}
